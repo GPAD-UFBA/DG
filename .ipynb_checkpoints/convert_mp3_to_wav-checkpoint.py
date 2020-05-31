4b78f2e6dab8920a5ef15bf507bdd0657d319073
Henrique Moura <henriquedemourasinezio@gmail.com>
2020-05-31 20:52:35 +0200

    Big update!
    Tradução de tudo pra inglês, organização de todas as funções num único módulo, e documentação das funções.

--- .ipynb_checkpoints/convert_mp3_to_wav-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-def convert(filename):
-    # Convert .mp3 to .wav
-    # filename = "example.mp3"
-    sound = AudioSegment.from_mp3(filename)
-    soundwav = sound.export(filename[:-4] + ".wav", format="wav")
-    soundwav.close()
--- plot_ODF.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def plot_ODF(ODF):
-    plt.figure()
-    plt.plot(ODF)
-    plt.title('ODF')
-    plt.ylabel('Onset Strength')
-    plt.xlabel('Amostras')
